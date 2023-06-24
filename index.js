fef873a76fb809581ea00baf6b9d57729bce1be3
Juangui <juan-ramirez04@hotmail.com>
2023-06-24 11:52:21 -0500

    Create functions in index.js

--- index.js
+++ index.js
@@ -2,13 +2,39 @@ async function callingAPI(){
     try {
         const response = await fetch("https://rickandmortyapi.com/api/character");
         const data = await response.json();
-        console.log(data);
-        const characters = data.results;
-        characters.forEach(character => {
-        });
+        return data.results;
     } catch (error) {
         console.log(error);
     }
 }
 
-document.querySelector(".button").addEventListener('click', callingAPI);
+const buildCard = (character) => {
+    const tempElement = document.createElement("div")
+    const htmlCard = `<li>
+                        <img src="${character.image}"/>
+                        <h2>${character.name} </h2>
+                        <p> ${character.species}</p>
+                    </li>`
+
+    tempElement.innerHTML = htmlCard
+    const [renderedCard] = tempElement.children
+    return renderedCard
+}
+
+
+
+async function printCharacters(characters){
+    const list = document.querySelector(".cardsList")
+    const cards = characters.map(buildCard)
+
+    cards.forEach(card => {
+        list.append(card)
+    });
+}
+
+
+// -----------------Funcion que se autollama(Se crea y se ejecuta ahi mismo)---------------
+(async () => {
+    const characters = await callingAPI();
+    printCharacters(characters)
+})()
