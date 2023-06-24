e515eb727f8bff07750a1bd41595998cc2a5b099
Andres <jandresrios0@gmail.com>
2023-06-24 10:01:18 -0500

    Add the evento to the button

--- index.js
+++ index.js
@@ -2,13 +2,13 @@ async function callingAPI(){
     try {
         const response = await fetch("https://rickandmortyapi.com/api/character");
         const data = await response.json();
+        console.log(data);
         const characters = data.results;
         characters.forEach(character => {
-            console.log(character.name);
         });
     } catch (error) {
         console.log(error);
     }
 }
 
-
+document.querySelector(".button").addEventListener('click', callingAPI);
